8f760ff69fc75fb0bfa6c5ef186771e9efae3e08
cberry <cberry@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2008-08-13 03:31:40 +0000

    create branch for 2.0.1.1

---
+++ branches/atomserver-2.0.1.1/src/main/java/org/atomserver/uri/FeedTarget.java
@@ -0,0 +1,59 @@
+/* Copyright (c) 2007 HomeAway, Inc.
+ *  All rights reserved.  http://www.atomserver.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.atomserver.uri;
+
+import org.apache.abdera.protocol.server.RequestContext;
+import org.apache.abdera.protocol.server.TargetType;
+import org.atomserver.AtomCategory;
+import org.atomserver.FeedDescriptor;
+import org.atomserver.utils.logic.BooleanExpression;
+
+import java.util.Set;
+
+/**
+ * A URITarget that specifically represents a Feed request
+ * @author Chris Berry  (chriswberry at gmail.com)
+ * @author Bryon Jacob (bryon at jacob.net)
+ */
+public class FeedTarget extends URITarget implements FeedDescriptor {
+    private final Set<BooleanExpression<AtomCategory>> categoriesQuery;
+    private final String workspace;
+    private final String collection;
+
+    public FeedTarget(RequestContext requestContext,
+                      final String workspace,
+                      final String collection,
+                      Set<BooleanExpression<AtomCategory>> categoriesQuery) {
+        super(TargetType.TYPE_COLLECTION, requestContext);
+        this.categoriesQuery = categoriesQuery;
+        this.workspace = workspace;
+        this.collection = collection;
+    }
+
+    public String getWorkspace() {
+        return workspace;
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public Set<BooleanExpression<AtomCategory>> getCategoriesQuery() {
+        return categoriesQuery;
+    }
+}
