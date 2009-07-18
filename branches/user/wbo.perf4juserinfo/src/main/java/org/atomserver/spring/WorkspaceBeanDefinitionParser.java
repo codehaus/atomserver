ca4b257fcdb594cb827e23e99eb0ca881c8bb190
wbo <wbo@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2009-07-18 11:12:34 +0000

    wbo.perf4juserinfo - creating branch

---
+++ branches/user/wbo.perf4juserinfo/src/main/java/org/atomserver/spring/WorkspaceBeanDefinitionParser.java
@@ -0,0 +1,41 @@
+/* Copyright (c) 2007 HomeAway, Inc.
+ *  All rights reserved.  http://www.atomserver.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.atomserver.spring;
+
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.w3c.dom.Element;
+import org.atomserver.core.WorkspaceOptions;
+
+/**
+ * @author Chris Berry  (chriswberry at gmail.com)
+ * @author Bryon Jacob (bryon at jacob.net)
+ */
+public class WorkspaceBeanDefinitionParser
+        extends AbstractSingleBeanDefinitionParser {
+
+    protected Class getBeanClass(Element element) {
+        return WorkspaceOptions.class; 
+    }
+
+    protected void doParse(Element element, BeanDefinitionBuilder bean) {
+        bean.addPropertyValue("name",
+                              element.getAttribute("name"));
+        bean.addPropertyValue("defaultLocalized",
+                              Boolean.valueOf(element.getAttribute("localized")));
+    }
+}
