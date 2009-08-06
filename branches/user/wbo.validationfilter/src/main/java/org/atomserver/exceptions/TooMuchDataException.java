e2e59d6ba966eda3d795263b82bbad3844d889b0
wbo <wbo@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2009-08-06 22:09:36 +0000

    Updated Javadocs

--- branches/user/wbo.validationfilter/src/main/java/org/atomserver/exceptions/TooMuchDataException.java
+++ branches/user/wbo.validationfilter/src/main/java/org/atomserver/exceptions/TooMuchDataException.java
@@ -1,10 +1,23 @@
-/* Copyright Homeaway, Inc 2005-2008. All Rights Reserved.
- * No unauthorized use of this software.
+/* Copyright (c) 2009 HomeAway, Inc.
+ *  All rights reserved.  http://www.atomserver.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.atomserver.exceptions;
 
 /**
- * wbo has not yet bothered to enter a useful javadoc comment.
+ * Exception that is thrown when the data size is too large to process.
  */
 public class TooMuchDataException extends RuntimeException {
     public TooMuchDataException() {
