787070188c77c70bdfab72c50253959a1519fd31
wbo <wbo@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2009-12-11 15:29:09 +0000

    wbo.diffcontent - creating branch

---
+++ branches/user/wbo.diffcontent/src/test/java/org/atomserver/core/dbstore/utils/SeederDBSTest.java
@@ -0,0 +1,110 @@
+/* Copyright (c) 2007 HomeAway, Inc.
+ *  All rights reserved.  http://www.atomserver.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.atomserver.core.dbstore.utils;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.atomserver.core.BaseFeedDescriptor;
+import org.atomserver.core.EntryMetaData;
+import org.atomserver.core.dbstore.dao.EntriesDAO;
+import org.atomserver.utils.conf.ConfigurationAwareClassLoader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Date;
+import java.util.List;
+
+public class SeederDBSTest extends TestCase {
+
+    static private Log log = LogFactory.getLog(SeederDBSTest.class);
+    static private String userDir = System.getProperty("user.dir");
+
+    static protected final Date ZERO_DATE = new Date(0L);
+
+    static private boolean hasRun = false;
+
+    static public boolean hasRun() { return hasRun; }
+
+    // -------------------------------------------------------
+    public static Test suite() { return new TestSuite(SeederDBSTest.class); }
+
+    // -------------------------------------------------------
+    protected void setUp() throws Exception { super.setUp(); }
+
+    // -------------------------------------------------------
+    protected void tearDown() throws Exception { super.tearDown(); }
+
+    public SeederDBSTest() {}
+
+    public SeederDBSTest(String name) { super(name); }
+
+    //----------------------------
+    //          Tests
+    //----------------------------
+    public void testSeedDB() throws Exception {
+
+        if (hasRun) {
+            return;
+        }
+
+        String[] configs = {
+                "/org/atomserver/spring/propertyConfigurerBeans.xml",
+                "/org/atomserver/spring/logBeans.xml",
+                "/org/atomserver/spring/storageBeans.xml",
+                "/org/atomserver/spring/databaseBeans.xml"
+        };
+        ClassPathXmlApplicationContext springFactory =
+                new ClassPathXmlApplicationContext(configs, false);
+        springFactory.setClassLoader(
+                new ConfigurationAwareClassLoader(springFactory.getClassLoader()));
+        springFactory.refresh();
+
+
+        try {
+            DBSeeder.getInstance(springFactory).seedEntriesClearingFirst();
+        } catch ( Exception ee ) {
+            ee.printStackTrace();
+        }
+        hasRun = true;
+
+        // We should now get an ordered List back
+        // SORTED -- From the beginning of time
+
+        EntriesDAO widgetsDAOiBatis = (EntriesDAO) springFactory.getBean("org.atomserver-entriesDAO");
+
+        List sortedList =
+                widgetsDAOiBatis.selectEntriesByLastModifiedSeqNum(new BaseFeedDescriptor("widgets", null), ZERO_DATE);
+        log.debug("List= " + sortedList);
+
+        Date lastVal = ZERO_DATE;
+        long seqNum = 0;
+        for (Object obj : sortedList) {
+            EntryMetaData widget = (EntryMetaData) obj;
+
+            assertTrue(lastVal.compareTo(widget.getUpdatedDate()) <= 0);
+            lastVal = widget.getUpdatedDate();
+
+            assertTrue( "expected " + seqNum + " < " + widget.getUpdateTimestamp() + " for " + widget,
+                        seqNum < widget.getUpdateTimestamp());
+
+            seqNum = widget.getUpdateTimestamp();
+        }
+    }
+}
---
+++ branches/user/wbo.diffcontent/src/test/java/org/atomserver/utils/test/TestingAtomServerTest.java
@@ -0,0 +1,194 @@
+/* Copyright (c) 2007 HomeAway, Inc.
+ *  All rights reserved.  http://www.atomserver.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.atomserver.utils.test;
+
+import junit.framework.TestCase;
+import org.apache.abdera.model.*;
+import org.apache.abdera.parser.stax.FOMParser;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.atomserver.testutils.latency.LatencyUtil;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Arrays;
+
+public class TestingAtomServerTest extends TestCase {
+    private static final String LOVEJOYS_CONTENT = "<bar xmlns=\"http://atomserver.org/bars\">Lovejoys</bar>";
+
+    // A "good" bar, for testing that we can put valid content into the service
+    private static final String LOVEJOYS =
+            "<entry xmlns=\"http://www.w3.org/2005/Atom\">\n" +
+            "  <id>bars/test/1234.xml</id>\n" +
+            "  <content type=\"xhtml\">\n" +
+            "    <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            "      " + LOVEJOYS_CONTENT + "\n" +
+            "    </div>\n" +
+            "  </content>\n" +
+            "</entry>";
+
+    // A "bad" bar, for testing that we reject things that don't match the RNC
+    private static final String AQUARIUM =
+            "<entry xmlns=\"http://www.w3.org/2005/Atom\">\n" +
+            "  <id>bars/test/1234.xml</id>\n" +
+            "  <content type=\"xhtml\">\n" +
+            "    <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            "      <bar xmlns=\"http://atomserver.org/bars\"><name>Aquarium</name></bar>\n" +
+            "    </div>\n" +
+            "  </content>\n" +
+            "</entry>";
+
+    // a BAZ to POST
+    private static final String BAZ =
+            "<entry xmlns=\"http://www.w3.org/2005/Atom\">\n" +
+            "  <content type=\"xhtml\">\n" +
+            "    <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+            "      <baz xmlns=\"http://atomserver.org/baz\">I am a baz</baz>\n" +
+            "    </div>\n" +
+            "  </content>\n" +
+            "</entry>";
+
+    // Long EntryId to PUT
+    private static final String LONGID =
+        "<entry xmlns=\"http://www.w3.org/2005/Atom\">\n" +
+        "  <id>bars/test/1111111111222222222233333333334444444444.xml</id>\n" +
+        "  <content type=\"xhtml\">\n" +
+        "    <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+        "      <bar xmlns=\"http://atomserver.org/bars\"><name>Aquarium</name></bar>\n" +
+        "    </div>\n" +
+        "  </content>\n" +
+        "</entry>";
+
+    // Long Term to PUT
+    private static final String LONGTERM =
+        "<entry xmlns=\"http://www.w3.org/2005/Atom\">\n" +
+        "  <id>tags:bars/test/1234</id>\n" +
+        "  <content type=\"application/xml\">\n" +
+        "    <app:categories xmlns:app=\"http://www.w3.org/2007/app\" xmlns=\"http://www.w3.org/2005/Atom\" >\n" +
+        "        <category scheme=\"urn:bars.test.type\" term=\"0000000000111111111122222222223333333333\" />\n" +
+        "    </app:categories> \n" +
+        "  </content>\n" +
+        "</entry>";
+
+    // test that our server works when configured through Spring
+    public void testSpringConfigured() throws Exception {
+        TestingAtomServer server = new TestingAtomServer();
+
+        server.setSpringBeansLocation("org/atomserver/utils/test/workspaces.xml");
+
+        runTests(server, "spring");
+    }
+
+    // test that our server works when configured through the API
+    public void testAPIConfigured() throws Exception {
+        TestingAtomServer server = new TestingAtomServer();
+
+        server.addWorkspace("bars", false)
+                .setRncLocation("classpath:org/atomserver/utils/test/bars.rnc")
+                .setXPathAutotaggerRules(
+                        "NAMESPACE bars = http://atomserver.org/bars;\n" +
+                        "DELETE SCHEME {urn:bar.name};\n" +
+                        "MATCH \"/bars:bar\" {urn:bar.name}$;");
+        server.addWorkspace("baz", true);
+
+        runTests(server, "api");
+    }
+
+    private void runTests(TestingAtomServer server, String collection) throws Exception {
+        int port = server.start("atomserver", "v1");
+
+        // create lovejoys
+        putEntry("http://localhost:" + port + "/atomserver/v1/bars/" + collection + "/lovejoys.xml",
+                 LOVEJOYS, 201);
+        // update lovejoys
+        putEntry("http://localhost:" + port + "/atomserver/v1/bars/" + collection + "/lovejoys.xml/1",
+                 LOVEJOYS, 200);
+        // should get an error from aquarium
+        putEntry("http://localhost:" + port + "/atomserver/v1/bars/" + collection + "/aquarium.xml",
+                 AQUARIUM, 422);
+
+        LatencyUtil.updateLastWrote();
+
+        // getting Lovejoys
+        Entry entry = get(Entry.class, "http://localhost:" + port + "/atomserver/v1/bars/" + collection + "/lovejoys.xml");
+        assertEquals(LOVEJOYS_CONTENT, entry.getContent());
+        assertEquals("Lovejoys", entry.getCategories("urn:bar.name").get(0).getTerm());
+
+        LatencyUtil.accountForLatency();
+
+        // the feed should have ONE entry - LoveJoys
+        Feed feed = get(Feed.class, "http://localhost:" + port + "/atomserver/v1/bars/" + collection + "?entry-type=full");
+        assertEquals(1, feed.getEntries().size());
+        assertEquals(LOVEJOYS_CONTENT, feed.getEntries().get(0).getContent());
+        assertEquals("Lovejoys", feed.getEntries().get(0).getCategories("urn:bar.name").get(0).getTerm());
+
+        // the service doc should have two workspaces - bars and baz
+        Service service = get(Service.class, "http://localhost:" + port + "/atomserver/v1");
+        assertEquals(2, service.getWorkspaces().size());
+        for (Workspace ws : service.getWorkspaces()) {
+            assertTrue(Arrays.asList("bars", "baz").contains(ws.getTitle()));
+        }
+
+        postEntry("http://localhost:" + port + "/atomserver/v1/baz/" + collection + "?locale=en_US",
+                 BAZ, 201);
+
+
+        putEntry("http://localhost:" + port + "/atomserver/v1/bars/" + collection + "/1111111111222222222233333333334444444444.xml",
+                 LONGID, 400);
+
+        putEntry("http://localhost:" + port + "/atomserver/v1/tags:bars/test/1234.xml/1",
+                 LONGTERM, 400);
+
+        server.stop();
+    }
+
+    private void putEntry(String url, String content, int expectedResponse) throws IOException {
+        HttpClient client = new HttpClient();
+        PutMethod put = new PutMethod(url);
+        put.setRequestHeader("Content-type", "text/xml; charset=UTF-8");
+
+        put.setRequestEntity(new StringRequestEntity(
+                content,
+                "application/xml",
+                "UTF-8"));
+
+        assertEquals(expectedResponse, client.executeMethod(put));
+    }
+
+    private void postEntry(String url, String content, int expectedResponse) throws IOException {
+        HttpClient client = new HttpClient();
+        PostMethod post = new PostMethod(url);
+        post.setRequestHeader("Content-type", "text/xml; charset=UTF-8");
+
+        post.setRequestEntity(new StringRequestEntity(
+                content,
+                "application/xml",
+                "UTF-8"));
+
+        assertEquals(expectedResponse, client.executeMethod(post));
+    }
+
+    private <T extends Element> T get(Class<T> clazz, String url) throws IOException {
+        HttpClient client = new HttpClient();
+        GetMethod get = new GetMethod(url);
+
+        client.executeMethod(get);
+        return new FOMParser().<T>parse(new StringReader(get.getResponseBodyAsString())).getRoot();
+    }
+}
