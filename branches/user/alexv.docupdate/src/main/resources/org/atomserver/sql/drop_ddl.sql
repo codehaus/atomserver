ea4ac90bfe87300eb4a825a6e2ae38e2ff6da7f5
alexvictoria <alexvictoria@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2010-02-18 22:08:03 +0000

    alexv.docupdate - deleting promoted branch

--- branches/user/alexv.docupdate/src/main/resources/org/atomserver/sql/drop_ddl.sql
+++
@@ -1,8 +0,0 @@
---
--- Drop Tables script 
---
-
-drop table if exists EntryCategoryLogEvent;
-drop table if exists EntryCategory;
-drop table if exists EntryStore;
-drop table if exists WorkspaceSequence;
