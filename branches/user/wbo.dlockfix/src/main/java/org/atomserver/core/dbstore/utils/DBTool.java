8d5f5692d19116fadb98c29c2e1662922b01e17e
wbo <wbo@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2010-02-22 20:16:23 +0000

    wbo.dlockfix - creating branch

---
+++ branches/user/wbo.dlockfix/src/main/java/org/atomserver/core/dbstore/utils/DBTool.java
@@ -0,0 +1,45 @@
+package org.atomserver.core.dbstore.utils;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.atomserver.utils.conf.ConfigurationAwareClassLoader;
+
+/**
+ * @author Chris Berry  (chriswberry at gmail.com)
+ * @author Bryon Jacob (bryon at jacob.net)
+ */
+public class DBTool {
+
+    public static ApplicationContext getToolContext(ApplicationContext parentContext) {
+        String[] configs = {
+                "/org/atomserver/spring/dbtools/dbtools.xml"
+        };
+        ClassPathXmlApplicationContext springFactory =
+                new ClassPathXmlApplicationContext(configs, false, parentContext);
+        springFactory.setClassLoader(
+                new ConfigurationAwareClassLoader(springFactory.getClassLoader()));
+        springFactory.refresh();
+        return springFactory;
+    }
+
+    public static ApplicationContext getToolContext() {
+
+        String[] configs = {
+                "/org/atomserver/spring/propertyConfigurerBeans.xml",
+                "/org/atomserver/spring/logBeans.xml",
+                "/org/atomserver/spring/storageBeans.xml",
+                "/org/atomserver/spring/abderaBeans.xml",
+                "/org/atomserver/spring/databaseBeans.xml",
+                "/org/atomserver/spring/dbtools/dbtools.xml",
+                "/org/atomserver/spring/ext/*.xml"
+        };
+
+        ClassPathXmlApplicationContext springFactory =
+                new ClassPathXmlApplicationContext(configs, false);
+        springFactory.setClassLoader(
+                new ConfigurationAwareClassLoader(springFactory.getClassLoader()));
+        springFactory.refresh();
+        return springFactory;
+    }
+
+}
