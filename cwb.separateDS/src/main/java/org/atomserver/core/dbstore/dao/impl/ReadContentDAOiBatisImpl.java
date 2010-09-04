bd3428a2093c861f9cb7b73d57791cec6281f2fd
cberry <cberry@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2010-09-04 13:05:25 +0000

    add content dao

---
+++ cwb.separateDS/src/main/java/org/atomserver/core/dbstore/dao/impl/ReadContentDAOiBatisImpl.java
@@ -0,0 +1,38 @@
+/* Copyright Homeaway, Inc 2005-2007. All Rights Reserved.
+ * No unauthorized use of this software.
+ */
+package org.atomserver.core.dbstore.dao.impl;
+
+import org.atomserver.core.EntryMetaData;
+import org.atomserver.core.dbstore.dao.ReadContentDAO;
+import org.atomserver.utils.perf.AtomServerPerfLogTagFormatter;
+import org.atomserver.utils.perf.AtomServerStopWatch;
+import org.perf4j.StopWatch;
+
+/**
+ *
+ */
+public class ReadContentDAOiBatisImpl
+        extends AbstractDAOiBatisImpl
+        implements ReadContentDAO {
+
+    public String selectContent(EntryMetaData entry) {
+        StopWatch stopWatch = new AtomServerStopWatch();
+        try {
+            return (String)
+                    getSqlMapClientTemplate().queryForObject("selectContent",
+                                                             paramMap().param("entryStoreId",
+                                                                              entry.getEntryStoreId()));
+        }
+        finally {
+            stopWatch.stop("DB.selectContent", AtomServerPerfLogTagFormatter.getPerfLogEntryString(entry));
+        }
+    }
+
+    public boolean contentExists(EntryMetaData entry) {
+        Integer count = (Integer)
+                getSqlMapClientTemplate().queryForObject("selectContentExists",
+                                                         paramMap().param("entryStoreId", entry.getEntryStoreId()));
+        return count > 0;
+    }
+}
