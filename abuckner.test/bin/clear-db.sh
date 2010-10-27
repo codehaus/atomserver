54e8e1e20384bb6f69152e2f16a6680a9b77aa15
mcslionhair <mcslionhair@0ca2fb25-484c-0410-9738-cfa17ccd6cc1>
2010-10-27 21:30:02 +0000

    abuckner.test - deleting promoted branch

--- abuckner.test/bin/clear-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## may not work for you
-##  depends on whether root is the DB owner
-
-echo "clear-db.sh..."
-echo "CLEARING DB TABLES IN atomserver_dev (PROMPTS FOR PASSWORD)"
-psql -U atomserver atomserver_dev < src/main/resources/org/atomserver/sql/delete_all.sql
-
